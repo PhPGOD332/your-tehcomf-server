3ebfdae0cae78aac5105697df9a1b80214002b5c
Timur <tima.gizatullin.01@mail.ru>
2025-07-30 23:52:56 +0500

    Исправлен конфиг для pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'your-tehcomf-server',
-      script: 'src/main.ts',
+      script: 'dist/main.ts',
       args: '-p 3001',
       exec_mode: 'fork',
       instances: '1'
