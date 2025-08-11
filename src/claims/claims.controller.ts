defe58dc0d4edf1096d17495a56e9f32063b39ca
Timur <tima.gizatullin.01@mail.ru>
2025-08-11 14:05:17 +0500

    Тестовая проверка GET маршрута claims/getClaims.

--- src/claims/claims.controller.ts
+++ src/claims/claims.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import {ClaimDto} from "../dtos/Claim.dto";
 import {ClaimsService} from "./claims.service";
 import {IClaim} from "../types/IClaim";
@@ -6,6 +6,17 @@ import {IClaim} from "../types/IClaim";
 @Controller('claims')
 export class ClaimsController {
     constructor(private readonly claimsService: ClaimsService) {}
+
+    @Get('getClaims')
+    getClaims(): IClaim {
+        return {
+            id: '1',
+            firstName: 'name',
+            phoneNumber: 'phone',
+            note: 'note',
+            dateCreated: 'date'
+        } as IClaim;
+    }
 
     @Post('addClaim')
     async addClaim(@Body() claimDto: ClaimDto): Promise<IClaim> {
