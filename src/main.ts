4f3183ee188cb5e3e3583818207899fc8d815503
Timur <tima.gizatullin.01@mail.ru>
2025-12-03 19:08:17 +0500

    Добавлена раздача статики в main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from "process";
+import {NestExpressApplication} from "@nestjs/platform-express";
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('api');
+
+  app.useStaticAssets(join(__dirname, '../../data/images'));
 
   app.enableCors({
     credentials: true,
